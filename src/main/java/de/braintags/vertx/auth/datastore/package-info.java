c5c4c9547fcb0dd871d35606ad5e9b12d4510eca
Jan Kerkenhoff <jan.kerkenhoff@braintags.de>
2016-10-24 16:39:40 +0200

    fixed asciidoc generation

--- src/main/java/de/braintags/vertx/auth/datastore/package-info.java
+++ src/main/java/de/braintags/vertx/auth/datastore/package-info.java
@@ -3,8 +3,11 @@
  * Authentication across an {@link de.braintags.io.vertx.pojomapper.IDataStore}
  * Crypt of passwords is ruled by mapper specifications by using the
  * {@link de.braintags.io.vertx.pojomapper.annotation.field.Encoder} annotation
- * 
+ *
  * @author Michael Remme
- * 
+ *
  */
+@Document(fileName = "index.adoc")
 package de.braintags.vertx.auth.datastore;
+
+import io.vertx.docgen.Document;
